ddb2b2760c1de12b4f895c011aa5ad5a649d834e
markjacobsen <mark@markjacobsen.net>
2013-04-23 17:54:40 -0400

    Added optional joinedFileNamePrefix param to batchMp3Append() and batchMp3Prepend()

--- src/com/cffreedom/utils/AudioUtils.java
+++ src/com/cffreedom/utils/AudioUtils.java
@@ -15,25 +15,36 @@ import com.cffreedom.file.FilterNameEnd;
  * 1) Donating: http://www.communicationfreedom.com/go/donate/
  * 2) Shoutout on twitter: @MarkJacobsen or @cffreedom
  * 3) Linking to: http://visit.markjacobsen.net
+ * 
+ * Changes:
+ * 2013-04-23 	markjacobsen.net 	Added optional joinedFileNamePrefix param to batchMp3Append() and batchMp3Prepend()
  */
 public class AudioUtils
 {
+	public static int batchMp3Append(String srcDir, String appendFile, String outputDir) {return batchMp3Append(srcDir, appendFile, outputDir, "");}
 	/**
 	 * Append appendFile to all the mp3 files in srcDir one at a time, and output the 
 	 * resulting files to outputDir
 	 * @param srcDir Directory containing mp3 files we want to prepend
 	 * @param appendFile mp3 file to append to each mp3 in srcDir
 	 * @param outputDir Directory to put joined files
+	 * @param joinedFileNamePrefix A string to prepend to the joined filename
+	 * @return The number of files joined
 	 */
-	public static void batchMp3Append(String srcDir, String appendFile, String outputDir)
+	public static int batchMp3Append(String srcDir, String appendFile, String outputDir, String joinedFileNamePrefix)
 	{
 		final String METHOD = "batchMp3Append";
+		int count = 0;
 		File dir = new File(srcDir);
 		File[] files = dir.listFiles(new FilterNameEnd(".mp3"));
+		
+		if (joinedFileNamePrefix == null) { joinedFileNamePrefix = ""; }
+		
 		for (File file : files)
 		{
+			count++;
 			String prependFile = file.getAbsolutePath();
-			String outputFile = outputDir + SystemUtils.getPathSeparator() + file.getName().replaceFirst(".mp3", "-joined.mp3");
+			String outputFile = outputDir + SystemUtils.getPathSeparator() + joinedFileNamePrefix + file.getName().replaceFirst(".mp3", "-joined.mp3");
 			if (FileUtils.fileExists(outputFile) == true)
 			{
 				LoggerUtil.log(METHOD, "WARN: Deleting: " + outputFile);
@@ -43,7 +54,11 @@ public class AudioUtils
 			String[] concatFiles = {prependFile, appendFile};
 			FileUtils.concatFiles(concatFiles, outputFile);
 		}
+		return count;
 	}
+	
+	
+	public static int batchMp3Prepend(String srcDir, String prependFile, String outputDir) { return batchMp3Prepend(srcDir, prependFile, outputDir, ""); } 
 	
 	/**
 	 * Prepend prependFile to all the mp3 files in srcDir one at a time, and output the 
@@ -51,16 +66,23 @@ public class AudioUtils
 	 * @param srcDir Directory containing mp3 files we want to append
 	 * @param prependFile mp3 file to prepend to each mp3 in srcDir
 	 * @param outputDir Directory to put joined files
+	 * @param joinedFileNamePrefix A string to prepend to the joined filename
+	 * @return The number of files joined
 	 */
-	public static void batchMp3Prepend(String srcDir, String prependFile, String outputDir)
+	public static int batchMp3Prepend(String srcDir, String prependFile, String outputDir, String joinedFileNamePrefix)
 	{
-		final String METHOD = "batchMp3Append";
+		final String METHOD = "batchMp3Prepend";
+		int count = 0;
 		File dir = new File(srcDir);
 		File[] files = dir.listFiles(new FilterNameEnd(".mp3"));
+		
+		if (joinedFileNamePrefix == null) { joinedFileNamePrefix = ""; }
+		
 		for (File file : files)
 		{
+			count++;
 			String appendFile = file.getAbsolutePath();
-			String outputFile = outputDir + SystemUtils.getPathSeparator() + file.getName().replaceFirst(".mp3", "-joined.mp3");
+			String outputFile = outputDir + SystemUtils.getPathSeparator() + joinedFileNamePrefix + file.getName().replaceFirst(".mp3", "-joined.mp3");
 			if (FileUtils.fileExists(outputFile) == true)
 			{
 				LoggerUtil.log(METHOD, "WARN: Deleting: " + outputFile);
@@ -70,5 +92,7 @@ public class AudioUtils
 			String[] concatFiles = {prependFile, appendFile};
 			FileUtils.concatFiles(concatFiles, outputFile);
 		}
+		
+		return count;
 	}
 }
