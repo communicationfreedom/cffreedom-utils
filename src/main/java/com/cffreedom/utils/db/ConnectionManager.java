b363ac8b73c37b3e8f224e81255a3e7807163c56
markjacobsen <mark@markjacobsen.net>
2013-07-15 15:00:19 -0400

    Replaced the use of KeyValueFileMgr with a properties file

--- src/main/java/com/cffreedom/utils/db/ConnectionManager.java
+++ src/main/java/com/cffreedom/utils/db/ConnectionManager.java
@@ -1,6 +1,11 @@
 package com.cffreedom.utils.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,16 +13,17 @@ import org.slf4j.LoggerFactory;
 import com.cffreedom.beans.DbConn;
 import com.cffreedom.exceptions.DbException;
 import com.cffreedom.utils.ConversionUtils;
-import com.cffreedom.utils.KeyValueFileMgr;
 import com.cffreedom.utils.SystemUtils;
 import com.cffreedom.utils.Utils;
 import com.cffreedom.utils.db.pool.ConnectionFactory;
 import com.cffreedom.utils.file.FileUtils;
+import com.cffreedom.utils.security.SecurityUtils;
 
 /**
  * Automated layer for accessing DB Connections that should guarantee that
  * the user is not prompted for any information.
  * 
+ * Original Class: com.cffreedom.utils.db.ConnectionManager
  * @author markjacobsen.net (http://mjg2.net/code)
  * Copyright: Communication Freedom, LLC - http://www.communicationfreedom.com
  * 
@@ -32,32 +38,33 @@ import com.cffreedom.utils.file.FileUtils;
  * Changes:
  * 2013-05-06 	markjacobsen.net 	Created
  * 2013-06-25 	markjacobsen.net 	Added connection pooling to getConnection()
+ * 2013-07-15	markjacobsne.net 	Replaced the use of KeyValueFileMgr with a properties file
  */
 public class ConnectionManager
 {
-	public static final String DEFAULT_FILE = SystemUtils.getMyCFConfigDir() + SystemUtils.getPathSeparator() + "dbconn.dat";
+	public static final String DEFAULT_FILE = SystemUtils.getMyCFConfigDir() + SystemUtils.getPathSeparator() + "dbconn.properties";
 	private static final Logger logger = LoggerFactory.getLogger("com.cffreedom.utils.db.ConnectionManager");
-	private KeyValueFileMgr kvfm = null;
+	private HashMap<String, DbConn> conns = new HashMap<String, DbConn>();
 	private String file = null;
 	private ConnectionFactory connFactory = null;
 	
-	public ConnectionManager() throws DbException
+	public ConnectionManager() throws DbException, IOException
 	{
 		this(ConnectionManager.DEFAULT_FILE);
 	}
 	
-	public ConnectionManager(String file) throws DbException
+	public ConnectionManager(String file) throws DbException, IOException
 	{
 		this(file, false);
 	}
 	
-	public ConnectionManager(boolean cacheConnections) throws DbException
+	public ConnectionManager(boolean cacheConnections) throws DbException, IOException
 	{
 		this(ConnectionManager.DEFAULT_FILE, cacheConnections);
 	}
 	
-	public ConnectionManager(String file, boolean cacheConnections) throws DbException
-	{
+	public ConnectionManager(String file, boolean cacheConnections) throws DbException, IOException
+	{		
 		this.loadConnectionFile(file);
 
 		if (cacheConnections == true)
@@ -67,7 +74,38 @@ public class ConnectionManager
 		}
 	}
 	
-	public void loadConnectionFile(String file) throws DbException
+	private void save()
+	{
+		ArrayList<String> lines = new ArrayList<String>();
+		logger.debug("Saving to file {}", this.getConnectionFile());
+		
+		lines.add("#--------------------------------------------------------------------------------------");
+		lines.add("# While it is not recommended, you can put usernames and passwords into this file.");
+		lines.add("# Passwords do need to be encrypted using the SecurityUtils class.");
+		lines.add("# It is suggested that you use the DbConnManager app in cffreedom-cl-apps to maintain");
+		lines.add("# this file.");
+		lines.add("#--------------------------------------------------------------------------------------");
+		lines.add("");
+		lines.add("keys=" + ConversionUtils.toDelimitedString(this.conns.keySet(), ","));
+		lines.add("");
+		
+		for (String entry : this.conns.keySet())
+		{
+			logger.trace(entry);
+			DbConn conn = this.getDbConn(entry);
+			lines.add(entry + ".db=" + conn.getDb());
+			lines.add(entry + ".type=" + conn.getType());
+			lines.add(entry + ".host=" + conn.getHost());
+			lines.add(entry + ".port=" + conn.getPort());
+			lines.add(entry + ".user=" + conn.getUser());
+			lines.add(entry + ".password=" + SecurityUtils.encryptDecrypt(conn.getPassword()));
+			lines.add("");
+		}
+		
+		FileUtils.writeLinesToFile(this.getConnectionFile(), lines);
+	}
+	
+	public void loadConnectionFile(String file) throws DbException, IOException
 	{
 		final String METHOD = "loadFile";
 		
@@ -75,7 +113,38 @@ public class ConnectionManager
 		{
 			logger.debug("Loading file: {}", file);
 			this.file = file;
-			this.kvfm = new KeyValueFileMgr(this.file);  // init so we can use it from other apps
+			
+			Properties props = new Properties();
+			FileInputStream in = new FileInputStream(this.file);
+			props.load(in);
+			in.close();
+			
+			String[] keys = props.getProperty("keys").split(",");
+			
+			for (String key : keys)
+			{
+				logger.debug(key);
+				String type = props.getProperty(key + ".type");
+				String host = props.getProperty(key + ".host");
+				String db = props.getProperty(key + ".db");
+				String port = props.getProperty(key + ".port");
+				String user = props.getProperty(key + ".user");
+				String password = props.getProperty(key + ".password");
+				
+				if (port == null) { port = "0"; }
+				
+				DbConn dbconn = new DbConn(BaseDAO.getDriver(type),
+										BaseDAO.getUrl(type, host, db), 
+										type,
+										host,
+										db,
+										ConversionUtils.toInt(port));
+				
+				if (user != null) { dbconn.setUser(user); }
+				if (password != null) { dbconn.setPassword(SecurityUtils.encryptDecrypt(password)); }
+
+				this.conns.put(key, dbconn);
+			}
 		}
 		else
 		{
@@ -92,27 +161,12 @@ public class ConnectionManager
 	
 	public boolean keyExists(String key)
 	{
-		return this.kvfm.keyExists(key);
+		return this.conns.containsKey(key);
 	}
 	
 	public DbConn getDbConn(String key)
 	{
-		String[] entryVals = this.kvfm.getEntryAsString(key).split("\\|");
-		String type = entryVals[0];
-		String db = entryVals[1];
-		String host = entryVals[2];
-		int port = 0;
-		if (Utils.isInt(entryVals[3]) == true)
-		{
-			port = ConversionUtils.toInt(entryVals[3]);
-		}
-		DbConn dbconn = new DbConn(BaseDAO.getDriver(type),
-									BaseDAO.getUrl(type, host, db, port),
-									type,
-									host,
-									db,
-									port);
-		return dbconn;
+		return this.conns.get(key);
 	}
 	
 	public boolean cacheConnections() { if (this.connFactory != null){ return true; }else{ return false; } }
@@ -138,29 +192,53 @@ public class ConnectionManager
 		}
 	}
 	
-	private String buildValString(DbConn dbconn)
-	{
-		return dbconn.getType() + "|" + dbconn.getDb() + "|" + dbconn.getHost() + "|" + dbconn.getPort();
-	}
-	
 	public boolean addConnection(String key, DbConn dbconn)
 	{
-		return this.kvfm.addEntry(key, this.buildValString(dbconn));
+		if (this.conns.containsKey(key) == false)
+		{
+			this.conns.put(key, dbconn);
+			this.save();
+			return true;
+		}
+		else
+		{
+			logger.error("A connection named {} already exists", key);
+			return false;
+		}
 	}
 	
 	public boolean updateConnection(String key, DbConn dbconn)
 	{
-		return this.kvfm.updateEntry(key, this.buildValString(dbconn));
+		deleteConnection(key);
+		return addConnection(key, dbconn);
 	}
 	
 	public boolean deleteConnection(String key)
 	{
-		return this.kvfm.removeEntry(key);
+		if (this.conns.containsKey(key) == true)
+		{
+			this.conns.remove(key);
+			this.save();
+			return true;
+		}
+		else
+		{
+			logger.error("A connection named {} does not exist");
+			return false;
+		}
 	}
 	
 	public void printKeys()
 	{
-		this.kvfm.printEntryKeys();
+		Utils.output("Keys");
+		Utils.output("======================");
+		if ((this.conns != null) && (this.conns.size() > 0))
+		{
+			for(String key : this.conns.keySet())
+			{
+				Utils.output(key);
+			}
+		}
 	}
 	
 	public void printConnInfo(String key)
