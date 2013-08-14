535c09d21fbe920ee61afec988c31605e9dacb33
markjacobsen <mark@markjacobsen.net>
2013-08-14 09:50:41 -0400

    More informative error handling

--- src/main/java/com/cffreedom/utils/security/SecurityCipher.java
+++ src/main/java/com/cffreedom/utils/security/SecurityCipher.java
@@ -1,6 +1,14 @@
 package com.cffreedom.utils.security;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
@@ -28,10 +36,13 @@ import org.slf4j.LoggerFactory;
  * 2013-07-15	markjacobsen.net 	Slight mods for greater backword compatibility with org.apache.commons.codec.binary.Base64
  * 2013-07-16 	markjacobsen.net 	Renamed from EncryptDecryptProxy
  * 2013-07-19 	markjacobsen.net 	Renamed from SecurityManager
+ * 2013-08-14 	markjacobsen.net 	More informative error handling
  */
 public class SecurityCipher
 {
 	private static final Logger logger = LoggerFactory.getLogger("com.cffreedom.utils.security.SecurityCipher");
+	private static final String ALGORITHM = "DES";
+	private static final String CHARSET = "UTF8";
 	private String secretKey = null;
 	private DESKeySpec keySpec = null;
 	private SecretKeyFactory keyFactory = null;
@@ -43,17 +54,18 @@ public class SecurityCipher
 		{
 			if (secretKey.length() < 16)
 			{
+				logger.debug("Padding secretKey");
 				secretKey += "----------------";
 				secretKey = secretKey.substring(0, 16);
 			}
 			this.secretKey = secretKey;
-			this.keySpec = new DESKeySpec(this.secretKey.getBytes("UTF8"));
-			this.keyFactory = SecretKeyFactory.getInstance("DES");
+			this.keySpec = new DESKeySpec(this.secretKey.getBytes(CHARSET));
+			this.keyFactory = SecretKeyFactory.getInstance(ALGORITHM);
 			this.key = this.keyFactory.generateSecret(this.keySpec);
 		}
-		catch (Exception e)
+		catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException | UnsupportedEncodingException e)
 		{
-			logger.error(e.getMessage());
+			logger.error(e.getClass().getSimpleName() + " - " + e.getMessage());
 		}
 	}
 	
@@ -61,14 +73,15 @@ public class SecurityCipher
 	{
 		try
 		{
-			byte[] cleartext = value.getBytes("UTF8");
-			Cipher cipher = Cipher.getInstance("DES"); // cipher is not thread safe
+			byte[] cleartext = value.getBytes(CHARSET);
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.ENCRYPT_MODE, this.key);
 			return Convert.toString(Base64.encodeBase64(cipher.doFinal(cleartext)));
 		}
-		catch (Exception e)
+		catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException | 
+				IllegalBlockSizeException | UnsupportedEncodingException e)
 		{
-			logger.error(e.getMessage());
+			logger.error(e.getClass().getSimpleName() + " - " + e.getMessage());
 			return null;
 		}
 	}
@@ -78,14 +91,15 @@ public class SecurityCipher
 		try
 		{
 			byte[] encrypedPwdBytes = Base64.decodeBase64(value.getBytes());
-			Cipher cipher = Cipher.getInstance("DES");// cipher is not thread safe
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
 			cipher.init(Cipher.DECRYPT_MODE, this.key);
 			byte[] plainTextPwdBytes = (cipher.doFinal(encrypedPwdBytes));
 			return Convert.toString(plainTextPwdBytes);
 		}
-		catch (Exception e)
+		catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException |
+				IllegalBlockSizeException e)
 		{
-			logger.error(e.getMessage());
+			logger.error(e.getClass().getSimpleName() + " - " + e.getMessage());
 			return null;
 		}
 	}
--- src/test/java/com/cffreedom/utils/security/SecurityCipherTest.java
+++ src/test/java/com/cffreedom/utils/security/SecurityCipherTest.java
@@ -8,13 +8,30 @@ import org.junit.Test;
 
 public class SecurityCipherTest
 {
-	private final String KEY = "somerandomvaluefortesting";
+	private final String KEY_16_PLUS = "somerandomvaluefortesting";
+	private final String KEY_SMALL = "secret";
 	private final String TEST_STRING = "this is my value";
 
 	@Test
 	public void testSecurityCipher() throws IOException
 	{
-		SecurityCipher edp = new SecurityCipher(KEY);
+		SecurityCipher edp = new SecurityCipher(KEY_16_PLUS);
+		String encrypted = edp.encrypt(TEST_STRING);
+		Assert.assertEquals(TEST_STRING, edp.decrypt(encrypted));
+	}
+	
+	@Test
+	public void testZeroLengthVal() throws IOException
+	{
+		SecurityCipher edp = new SecurityCipher(KEY_16_PLUS);
+		String encrypted = edp.encrypt("");
+		Assert.assertEquals("", edp.decrypt(encrypted));
+	}
+
+	@Test
+	public void testSecurityCipherSmallKey() throws IOException
+	{
+		SecurityCipher edp = new SecurityCipher(KEY_SMALL);
 		String encrypted = edp.encrypt(TEST_STRING);
 		Assert.assertEquals(TEST_STRING, edp.decrypt(encrypted));
 	}
