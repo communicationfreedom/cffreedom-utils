6b4ee6b31841647e87516ef9fcf084e374ec6d2e
mjacobsen <mark@markjacobsen.net>
2015-08-09 23:03:27 -0400

    Update

--- src/main/java/com/cffreedom/utils/file/FileUtils.java
+++ src/main/java/com/cffreedom/utils/file/FileUtils.java
@@ -561,42 +561,47 @@ public class FileUtils
 	{
 		String[] ret = new String[0];
 		File dir = new File(folder);
-		File[] files = dir.listFiles(new DirFilter(filter));
+		File[] files = null;
 		
-		if (files != null)
-		{
-			if (files.length == 1)
+		if (FileUtils.folderExists(folder) == false) {
+			logger.warn("{} is not a valid folder", folder);
+		} else {
+			files =  dir.listFiles(new DirFilter(filter));
+			if (files != null)
 			{
-				ret = new String[]{ files[0].getName() };
-			}
-			else if (files.length > 1)
-			{
-				if (sort != null)
+				if (files.length == 1)
 				{
-					if (sort.equals(SORT.DATE) == true) {
-						Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_COMPARATOR);
-					} else if (sort.equals(SORT.DATE_DECENDING) == true) {
-						Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
-					} else if (sort.equals(SORT.NAME) == true) {
-						Arrays.sort(files, NameFileComparator.NAME_COMPARATOR);
-					} else if (sort.equals(SORT.NAME_DECENDING) == true) {
-						Arrays.sort(files, NameFileComparator.NAME_REVERSE);
+					ret = new String[]{ files[0].getName() };
+				}
+				else if (files.length > 1)
+				{
+					if (sort != null)
+					{
+						if (sort.equals(SORT.DATE) == true) {
+							Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_COMPARATOR);
+						} else if (sort.equals(SORT.DATE_DECENDING) == true) {
+							Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+						} else if (sort.equals(SORT.NAME) == true) {
+							Arrays.sort(files, NameFileComparator.NAME_COMPARATOR);
+						} else if (sort.equals(SORT.NAME_DECENDING) == true) {
+							Arrays.sort(files, NameFileComparator.NAME_REVERSE);
+						}
 					}
+					
+					List<String> result = new ArrayList<String>();
+					for (File file : files){
+				    	result.add(file.getName());
+				    }
+					ret = result.toArray(new String[result.size()]);
 				}
 				
-				List<String> result = new ArrayList<String>();
-				for (File file : files){
-			    	result.add(file.getName());
-			    }
-				ret = result.toArray(new String[result.size()]);
-			}
-			
-			if ((includeFullPath == true) && (ret.length > 0))
-			{
-				for (int x = 0; x < ret.length; x++)
+				if ((includeFullPath == true) && (ret.length > 0))
 				{
-					File file = new File(folder, ret[x]);
-					ret[x] = file.getAbsolutePath();
+					for (int x = 0; x < ret.length; x++)
+					{
+						File file = new File(folder, ret[x]);
+						ret[x] = file.getAbsolutePath();
+					}
 				}
 			}
 		}
