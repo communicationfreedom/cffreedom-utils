e000083449dd30dd9028527c892e7bb5d2e428a9
Mark <mark@markjacobsen.net>
2016-11-01 20:13:14 -0400

    junit test

--- src/test/java/com/cffreedom/utils/DateTimeUtilsTest.java
+++ src/test/java/com/cffreedom/utils/DateTimeUtilsTest.java
@@ -15,8 +15,7 @@ public class DateTimeUtilsTest
 	private static final Logger LOG = LoggerFactory.getLogger(DateTimeUtilsTest.class);
 	
 	@Test
-	public void testDayOfYear() throws IOException
-	{
+	public void testDayOfYear() throws IOException {
 		Date date = Convert.toDate("01/02/2012");
 		int doy = DateTimeUtils.dayOfYear(date);
 		assertEquals(2, doy);
@@ -27,8 +26,7 @@ public class DateTimeUtilsTest
 	}
 
 	@Test
-	public void testMonth()
-	{
+	public void testMonth() {
 		assertEquals(DateTimeUtils.month("January"), 1);
 		assertEquals(DateTimeUtils.month("feb"), 2);
 		assertEquals(DateTimeUtils.month("lkasdflj"), -1);
@@ -40,5 +38,13 @@ public class DateTimeUtilsTest
 		Calendar noTime = DateTimeUtils.stripTime(today);
 		//LOG.debug("{}-{}", today.get(Calendar.MONTH), noTime.get(Calendar.MONTH));
 		assertEquals(today.get(Calendar.MONTH), noTime.get(Calendar.MONTH));
+	}
+	
+	@Test
+	public void testEquality() {
+		Calendar one = DateTimeUtils.setDateTime(2015, 10, 3, 5, 23, 9, 2);
+		Calendar two = DateTimeUtils.setDateTime(2015, 10, 7, 15, 32, 17, 2);
+		assertTrue(DateTimeUtils.datesEqual(DateTimeUtils.dateAdd(one, 4, DateTimeUtils.DATE_PART_DAY), two, true));
+		assertFalse(DateTimeUtils.datesEqual(DateTimeUtils.dateAdd(one, 4, DateTimeUtils.DATE_PART_DAY), two, false));
 	}
 }
