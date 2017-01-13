c0e7da47ce1052c5e511ec83360a83f9593d8f1b
Mark <mark@markjacobsen.net>
2017-01-12 22:14:45 -0500

    Updated Cacher

--- src/main/java/com/cffreedom/utils/Cacher.java
+++ src/main/java/com/cffreedom/utils/Cacher.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 /**
  * Original Class: com.cffreedom.utils.Cacher
- * @author markjacobsen.net (http://mjg2.net/code)
+ * @author MarkJacobsen.net
  * Copyright: Communication Freedom, LLC - http://www.communicationfreedom.com
  * 
  * Free to use, modify, redistribute.  Must keep full class header including 
@@ -19,14 +19,14 @@ import java.util.Set;
  * If this helped you out or saved you time, please consider...
  * 1) Donating: http://www.communicationfreedom.com/go/donate/
  * 2) Shoutout on twitter: @MarkJacobsen or @cffreedom
- * 3) Linking to: http://visit.markjacobsen.net
+ * 3) Linking to: http://markjacobsen.net
  * 
  * Changes:
- * 2014-04-28 	markjacobsen.net 	Created
+ * 2014-04-28 	MarkJacobsen.net 	Created
  * 2015-10-27   MarkJacobsen.net 	Added maintainOrder option
+ * 2017-01-12 	MarkJacobsen.net 	More aggressive cleanup of expired items
  */
-public class Cacher
-{
+public class Cacher {
 	public static final int DEFAULT_CACHE_MIN = 60;
 	private static int defaultCacheMin = DEFAULT_CACHE_MIN;
 	private Map<String, CachedObject> cache = new HashMap<String, CachedObject>();
@@ -45,8 +45,7 @@ public class Cacher
 	 * @param key
 	 * @param obj
 	 */
-	public void put(String key, Object obj)
-	{
+	public void put(String key, Object obj) {
 		this.put(key, obj, defaultCacheMin);
 	}
 	
@@ -56,10 +55,10 @@ public class Cacher
 	 * @param obj
 	 * @param cacheMinutes
 	 */
-	public void put(String key, Object obj, int cacheMinutes)
-	{
-		CachedObject newObj = new CachedObject(key, obj, cacheMinutes);
+	public void put(String key, Object obj, int cacheMinutes) {
+		CachedObject newObj = new CachedObject(obj, cacheMinutes);
 		cache.put(key, newObj);
+		this.cleanupExpiredItems();
 	}
 	
 	/**
@@ -67,27 +66,18 @@ public class Cacher
 	 * @param key
 	 * @return
 	 */
-	public <T extends Object> T get(String key)
-	{
+	public <T extends Object> T get(String key)	{
+		this.cleanupExpiredItems();
 		CachedObject obj = cache.get(key);
 		
-		if (obj == null)
-		{
+		if (obj == null) {
 			return null;
-		}
-		else if (obj.isExpired() == true)
-		{
-			this.remove(key);
-			return null;
-		}
-		else
-		{
+		} else {
 			return (T)obj.getObj();
 		}
 	}
 	
-	public int size()
-	{
+	public int size() {
 		this.cleanupExpiredItems();
 		return cache.size();
 	}
@@ -95,8 +85,7 @@ public class Cacher
 	/**
 	 * Clear all cached items
 	 */
-	public void clear()
-	{
+	public void clear()	{
 		cache = new HashMap<String, CachedObject>();
 	}
 	
@@ -104,8 +93,7 @@ public class Cacher
 	 * Remove a specific item from the cache
 	 * @param key
 	 */
-	public void remove(String key)
-	{
+	public void remove(String key) {
 		cache.remove(key);
 	}
 	
@@ -118,8 +106,7 @@ public class Cacher
 	 * @param key
 	 * @return
 	 */
-	public boolean containsKey(String key)
-	{
+	public boolean containsKey(String key) {
 		// Note: Using call to cache.get() instead of directly calling containsKey()
 		// because the call to get() will expire the item if necessary
 		if (cache.get(key) == null) {
@@ -132,21 +119,17 @@ public class Cacher
 	/**
 	 * Cleanup (remove) any expired cached objects
 	 */
-	public void cleanupExpiredItems()
-	{
+	public void cleanupExpiredItems() {
 		// Done as a 2 step process to eliminate concurrency issues
 		List<String> removeKeys = new ArrayList<String>();
 		
-		for (String key : cache.keySet())
-		{
-			if (cache.get(key).isExpired() == true)
-			{
+		for (String key : cache.keySet()) {
+			if (cache.get(key).isExpired() == true) {
 				removeKeys.add(key);
 			}
 		}
 		
-		for (String key : removeKeys)
-		{
+		for (String key : removeKeys) {
 			this.remove(key);
 		}
 	}
@@ -157,14 +140,10 @@ public class Cacher
 	 * @param key
 	 * @return
 	 */
-	public Calendar getCachedTime(String key)
-	{
-		if (this.containsKey(key) == true)
-		{
+	public Calendar getCachedTime(String key) {
+		if (this.containsKey(key) == true) {
 			return cache.get(key).getCacheTime();
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
@@ -172,25 +151,19 @@ public class Cacher
 	/**
 	 * Print out information about the cache
 	 */
-	public void printCache()
-	{
-		for (String key : cache.keySet())
-		{
+	public void printCache() {
+		for (String key : cache.keySet()) {
 			CachedObject obj = cache.get(key);
 			Utils.output(key + " - cached: " + Format.date(Format.DATE_TIMESTAMP, Convert.toDate(obj.getCacheTime())) + " - Cache minutes: " + obj.getCacheMinutes());
 		}
 	}
 	
-	class CachedObject
-	{
-		private String key = null;
+	class CachedObject {
 		private Object obj = null;
 		private Calendar cached = Calendar.getInstance();
 		private int cacheMinutes = 0;
 		
-		public CachedObject(String key, Object obj, int cacheMinutes)
-		{
-			this.key = key;
+		public CachedObject(Object obj, int cacheMinutes) {
 			this.obj = obj;
 			this.cached = Calendar.getInstance();
 			this.cacheMinutes = cacheMinutes;
@@ -200,8 +173,7 @@ public class Cacher
 		public Calendar getCacheTime() { return this.cached; }
 		public int getCacheMinutes() { return this.cacheMinutes; }
 		
-		public boolean isExpired()
-		{
+		public boolean isExpired() {
 			Calendar testDate = DateTimeUtils.dateAdd(Calendar.getInstance(), this.cacheMinutes * -1, DateTimeUtils.DATE_PART_MINUTE);
 			return this.cached.before(testDate);	
 		}
