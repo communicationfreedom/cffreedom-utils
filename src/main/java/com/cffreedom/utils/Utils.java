92e74b7256610936e07674c672ccbaa877dfb9e2
Mark <mark@markjacobsen.net>
2018-08-13 07:15:38 -0400

    isEmail update

--- src/main/java/com/cffreedom/utils/Utils.java
+++ src/main/java/com/cffreedom/utils/Utils.java
@@ -167,6 +167,7 @@ public class Utils
     public static boolean isEmail(String val) {
     	boolean result = false;
     	if 	(
+    		(val != null) &&
     		(val.length() > 7) && 
     		val.contains("@") && 
     		val.contains(".") &&
